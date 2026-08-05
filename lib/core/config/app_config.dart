3d5c325f70dde6cc48f0de08214f047d4f9fbb2c
sony12subagyo <sonysubagyo0852@gmail.com>
2026-08-05 11:14:52 +0700

    merubah api

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,5 +4,5 @@ class AppConfig {
   static const String baseUrl =
       // api yang sudah di hosting
       //"https://amiflow-be-deploy-production.up.railway.app/api";
-      "https://henrietta-unsized-tillie.ngrok-free.dev/api";
+      "https://6804-203-190-44-108.ngrok-free.app/api";
 }
