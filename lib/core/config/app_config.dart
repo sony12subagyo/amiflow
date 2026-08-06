15b30c1f5fe5b44b5510ae8bc2ce425a37999138
haekalzanziban <haekalzanziban@gmail.com>
2026-08-06 11:53:20 +0700

    git rebase --continueanu API

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,5 +4,5 @@ class AppConfig {
   static const String baseUrl =
       // api yang sudah di hosting
       //"https://amiflow-be-deploy-production.up.railway.app/api";
-      "https://ether-undrafted-guileless.ngrok-free.dev/api";
+      "https://henrietta-unsized-tillie.ngrok-free.dev/api";
 }
