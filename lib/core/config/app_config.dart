e9541ddc924716670d12ba33669150a8eeedfd65
sony12subagyo <sonysubagyo0852@gmail.com>
2026-08-08 13:41:56 +0700

    update android manifest

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,6 +1,4 @@
 class AppConfig {
-  // Alamat backend lewat ngrok.
-  // GANTI setiap kali ngrok di-restart (alamatnya berubah di versi gratis).
   static const String baseUrl =
        //"https://021d-182-2-37-46.ngrok-free.app/api";
        "https://amiflow.nusaekateknologi.com/api";
